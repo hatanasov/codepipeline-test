d7a791b17de31d4353ecbf157f456eb287e843d0
Hristo Atanasov <hristo.atanasov@helecloud.com>
2022-02-09 10:56:07 +0200

    Fix repo structure

--- codebuild.tf
+++ codebuild.tf
@@ -29,11 +29,6 @@ resource "aws_codebuild_project" "build_image" {
     }
 
     environment_variable {
-      name  = "SOLUTION_DIR"
-      value = "build_image"
-    }
-
-    environment_variable {
       name  = "IMAGE_TAG"
       value = "latest"
     }
